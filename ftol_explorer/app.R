31bedce19d1079fd860917475c4097532e54b6b5
Joel Nitta <joelnitta@gmail.com>
2022-06-30 17:22:44 +0900

    Refactor app
    - split into data and usage panels
    - fix problem of multiple name matches per species

--- ftol_explorer/app.R
+++ ftol_explorer/app.R
@@ -2,6 +2,7 @@ library(shiny)
 library(shiny.react)
 library(shiny.fluent)
 library(tidyverse)
+library(assertr)
 
 source("functions.R")
 
@@ -11,10 +12,17 @@ match_results_resolved_all <- readRDS("data/match_results_resolved_all.RDS")
 
 species <- sort(sanger_accessions_selection$species)
 
-options <- NULL
+species_options <- NULL
 for (i in seq_along(species)) {
-  options[[i]] <- list(key = species[[i]], text = species[[i]])
+  species_options[[i]] <- list(key = species[[i]], text = species[[i]])
 }
+
+data_table_settings <- list(
+      dom = "t",
+      ordering = FALSE,
+      paging = FALSE,
+      searching = FALSE
+    )
 
 ui <- fluidPage(
 
@@ -22,29 +30,75 @@ ui <- fluidPage(
 
   sidebarLayout(
 
+    # Sidebar with input selection box for species
     sidebarPanel(
       # Use combo-box input for autocomplete
       ComboBox.shinyInput("combo", value = list(text = species[[1]]),
-        options = options, allowFreeform = TRUE
+        options = species_options, allowFreeform = TRUE
       )
     ),
-
+    # Main panel with output in three panels
     mainPanel(
-      dataTableOutput("view")
+
+      tabsetPanel(
+        tabPanel(
+          "Data",
+          fluidRow(
+            column(6,
+              markdown("### Accession"),
+              dataTableOutput("acc_data")
+            )
+          ),
+          fluidRow(
+            column(8,
+              markdown("### Voucher"),
+              dataTableOutput("voucher_data")
+            )
+          ),
+          fluidRow(
+            column(12,
+              markdown("### Taxonomy"),
+              dataTableOutput("taxonomy_data")
+            )
+          )
+        ),
+        tabPanel(
+          "How to use",
+          includeMarkdown("assets/explorer_explanation.md")
+        )
+      )
     )
   )
-
 )
 
 server <- function(input, output) {
-  output$view <- renderDataTable({
+
+  data_lists <- reactive({
     get_acc_info(
-    input$combo$text,
-    sanger_accessions_selection,
-    ncbi_accepted_names_map,
-    match_results_resolved_all)
-  }, escape = FALSE
+    species_select = input$combo$text,
+    accessions_long = ftolr::accessions_long,
+    accessions_wide = ftolr::accessions_wide,
+    match_results_resolved_all = match_results_resolved_all)
+  })
+
+  output$acc_data <- renderDataTable(
+    data_lists()$acc_data,
+    options = data_table_settings,
+    escape = FALSE
   )
+
+  output$voucher_data <- renderDataTable(
+    data_lists()$voucher_data,
+    options = data_table_settings,
+    escape = FALSE
+  )
+
+  output$taxonomy_data <- renderDataTable(
+    data_lists()$taxonomy_data,
+    options = data_table_settings,
+    escape = FALSE
+  )
+
 }
 
 shinyApp(ui = ui, server = server)
---
+++ ftol_explorer/assets/explorer_explanation.md
@@ -0,0 +1,27 @@
+## Getting started
+
+Start by selecting a species from the drop-down menu on the left.
+
+You can also type into the search box and it will auto-complete.
+
+### Accession
+
+This panel shows one row per accession for the selected species. 
+
+Columns are as follows:
+- **accession**: GenBank accession number; clicking will open a browser tab with that accession in GenBank.
+- **seq_len**: Length of the sequence in basepairs.
+
+### Voucher
+
+This panel has only one row.
+
+Columns are as follows:
+- **join_by**: Method used to join sequences across loci within the species.
+
+### Taxonomy
+
+This panel may have just one row if the data are the same for all accessions, or one row per accession if different accessions have different data.
+
+Columns are as follows:
+- **ncbi_taxid**: NCBI taxonomic ID for the accession (or species). Clicking on the ID number will open a browser tab with that ID in the NCBI Taxonomy database.
